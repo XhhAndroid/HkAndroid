16f8761e039664d9ea24791e023cdfd0184503cb
hay <hay@youxiang.io>
2022-08-06 17:21:06 +0800

    添加相关方法

--- handroid/src/main/java/com/h/android/utils/system/PhoneSystemUtil.kt
+++ handroid/src/main/java/com/h/android/utils/system/PhoneSystemUtil.kt
@@ -1,9 +1,14 @@
 package com.h.android.utils.system
 
+import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.Proxy
 import android.net.Uri
 import android.os.Build
+import android.text.TextUtils
+import com.h.android.HAndroid
 
 /**
  * @author zhangxiaohui
@@ -33,5 +38,51 @@ class PhoneSystemUtil {
         }
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
         context.startActivity(intent)
+    }
+
+    /**
+     * 判断当前应用是否在前台
+     * 系统签名的应用，可以获取其他应用进程，当前是否在前台 ，非系统签名应用只能获取自己的
+     */
+    fun isRunningForeground(context: Context): Boolean {
+        val activityManager = context.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val runningAppProcesses = activityManager.runningAppProcesses ?: return false
+        val packageName = context.applicationInfo.packageName
+        for (runningAppProcessInfo in runningAppProcesses) {
+            if (packageName.equals(
+                    runningAppProcessInfo.processName,
+                    ignoreCase = true
+                ) && runningAppProcessInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND
+            ) {
+                return true
+            }
+        }
+        return false
+    }
+
+    fun checkVPN(): Boolean {
+        //don't know why always returns null:
+        val connMgr = HAndroid.getApplication()
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connMgr.getNetworkInfo(ConnectivityManager.TYPE_VPN)
+        return networkInfo?.isConnected ?: false
+    }
+
+    /*
+    * 判断设备是否正在抓包
+    * */
+    fun isWifiProxy(context: Context): Boolean {
+        val IS_ICS_OR_LATER: Boolean = Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH
+        val proxyAddress: String
+        val proxyPort: Int
+        if (IS_ICS_OR_LATER) {
+            proxyAddress = if(System.getProperty("http.proxyHost") == null) "" else System.getProperty("http.proxyHost")
+            val portStr = System.getProperty("http.proxyPort")
+            proxyPort = (portStr ?: "-1").toInt()
+        } else {
+            proxyAddress = Proxy.getHost(context)
+            proxyPort = Proxy.getPort(context)
+        }
+        return !TextUtils.isEmpty(proxyAddress) && proxyPort != -1
     }
 }
